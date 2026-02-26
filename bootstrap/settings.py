9dcfb948d491eb9cd3c9a8ae4aee29f5a8e5f7cf
Bhashini367 <24b01a1217@svecw.edu.in>
2026-02-26 10:24:08 +0530

    changes done in settings.py to collect static files

--- bootstrap/settings.py
+++ bootstrap/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = '/home/Bhashini/college_website/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
